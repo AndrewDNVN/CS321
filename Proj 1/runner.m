d8653a757fb3d951801e852c426f1e28d353f9b5
Andrew <68348312+AndrewDnvn@users.noreply.github.com>
2022-02-10 11:44:54 -0500

    Fixing

--- Proj 1/runner.m
+++ Proj 1/runner.m
@@ -2,27 +2,46 @@
 % Andrew Donovan
 % CS 321
 % Runner code
-
+format long
 n = 3;
 A = randn(n,n);
 
 %our goal result for Gaussian functions
-Goal_Result = [1;2;3];
+b = randn(n);
 
-%b is found by multiplying A and our goal result
-b = zeros(1,n);
-b = A * Goal_Result;
-
+% run calculations using Naive Gauss
 outP1 = Gauss_Naive(A, b);
 
+% run calculations using Pivoted Gauss
 outP2 = Gauss_Pivoted(A, b);
 
-r_Naive = (A * outP1) - b;
-r_Pivoted = (A * outP2) - b;
+% calculate the residual vector for naive gauss
+r1 = (A * outP1) - b;
 
-norm_Naive = norm(r_Naive)
-norm_Pivoted = norm(r_Pivoted)
+% calculate the residual vector for Pivoted Gauss
+r2 = (A * outP2) - b;
 
+% norm the outputs and compare
+norm_Naive = norm(r1);
+norm_Pivoted = norm(r2);
 
+% Print out performance of the methods to the terminal
+
+if (norm_Naive > norm_Pivoted)
+    fprintf("The Naive Gaussian Elemination had a larger normilized value.\n");
+    fprintf("The Naive Gaussian normlized value: %d \n", norm_Naive);
+    fprintf("The Pivoted Gaussian normlized value: %d \n", norm_Pivoted);
+
+elseif (norm_Naive < norm_Pivoted)
+    fprintf("The Naive Gaussian Elemination had a smaller normilized value.\n");
+    fprintf("The Naive Gaussian normlized value: %d \n", norm_Naive);
+    fprintf("The Pivoted Gaussian normlized value: %d \n", norm_Pivoted);
+
+elseif (norm_Naive == norm_Pivoted)
+    fprintf("The Naive Gaussian Elemination had the same normilized value.\n");
+    fprintf("The Naive Gaussian normlized value: %d \n", norm_Naive);
+    fprintf("The Pivoted Gaussian normlized value: %d \n", norm_Pivoted);
+    
+end
 
 
