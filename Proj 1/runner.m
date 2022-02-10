505b0b7e207b293457ad71c2fab5c9f3b636c90d
Andrew <68348312+AndrewDnvn@users.noreply.github.com>
2022-02-10 11:48:44 -0500

    output cleaning

--- Proj 1/runner.m
+++ Proj 1/runner.m
@@ -3,7 +3,7 @@
 % CS 321
 % Runner code
 format long
-n = 3;
+n = 10000;
 A = randn(n,n);
 
 %our goal result for Gaussian functions
@@ -29,19 +29,17 @@ norm_Pivoted = norm(r2);
 
 if (norm_Naive > norm_Pivoted)
     fprintf("The Naive Gaussian Elemination had a larger normilized value.\n");
-    fprintf("The Naive Gaussian normlized value: %d \n", norm_Naive);
-    fprintf("The Pivoted Gaussian normlized value: %d \n", norm_Pivoted);
+    fprintf("The Naive Gaussian normlized value:\t\t %d \n", norm_Naive);
+    fprintf("The Pivoted Gaussian normlized value:\t %d \n", norm_Pivoted);
 
 elseif (norm_Naive < norm_Pivoted)
     fprintf("The Naive Gaussian Elemination had a smaller normilized value.\n");
-    fprintf("The Naive Gaussian normlized value: %d \n", norm_Naive);
-    fprintf("The Pivoted Gaussian normlized value: %d \n", norm_Pivoted);
+    fprintf("The Naive Gaussian normlized value:\t\t %d \n", norm_Naive);
+    fprintf("The Pivoted Gaussian normlized value:\t %d \n", norm_Pivoted);
 
 elseif (norm_Naive == norm_Pivoted)
     fprintf("The Naive Gaussian Elemination had the same normilized value.\n");
-    fprintf("The Naive Gaussian normlized value: %d \n", norm_Naive);
-    fprintf("The Pivoted Gaussian normlized value: %d \n", norm_Pivoted);
-    
+    fprintf("The Naive Gaussian normlized value:\t\t %d \n", norm_Naive);
+    fprintf("The Pivoted Gaussian normlized value:\t %d \n", norm_Pivoted);
+
 end
-
-
